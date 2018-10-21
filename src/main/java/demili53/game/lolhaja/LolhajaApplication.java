79bc12f4be0dbcc5e94766cdb99feb20a4191bdd
demili53 <demili5384@gmail.com>
2018-10-21 15:19:20 +0900

    스타트

---
+++ src/main/java/demili53/game/lolhaja/LolhajaApplication.java
@@ -0,0 +1,12 @@
+package demili53.game.lolhaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolhajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolhajaApplication.class, args);
+	}
+}
---
+++ src/test/java/demili53/game/lolhaja/LolhajaApplicationTests.java
@@ -0,0 +1,16 @@
+package demili53.game.lolhaja;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LolhajaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
